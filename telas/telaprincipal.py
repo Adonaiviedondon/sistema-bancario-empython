3585b2742381f6f0673cdb9e137bc73258eae0df
adonaiviedondon <vieiraadonai@gmail.com>
2024-07-21 20:35:21 -0300

    criação tela principal

---
+++ telas/telaprincipal.py
@@ -0,0 +1,14 @@
+import ttkbootstrap  as ttk
+from ttkbootstrap.constants import*
+from ttkbootstrap.style import Style
+
+tela2=ttk.Window("telausuario")
+tela2.geometry("600x500")
+estilo=Style(theme="superhero")
+
+
+
+
+
+
+tela2.mainloop()
